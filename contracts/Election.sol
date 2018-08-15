0e2d1cedb4ba02a60a2512ad3600722a22815a69
JESSTADA <jesstada@gmail.com>
2018-08-15 17:24:54 +0100

    added test to check number of candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -4,12 +4,26 @@ contract Election {
 	//Store value
 	//Read candidate
 	//Constructor
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	mapping(uint => Candidate) public candidates;
+	//candidates works like a simple function here, that looks up key value pairs 
+	uint public candidatesCount;
 
 	string public candidate;
 
 	function Election() public {
-		candidate = "Jess";
+		addCandidate("Candidate1");
+		addCandidate("Candidate2");
+	}
 
+	function addCandidate (string _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 
 
